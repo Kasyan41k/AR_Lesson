f3e1c24a8d03cc8211880a8255794c5c1fa314d9
Ruslan_Kasyanovich <ruslank906@gmail.com>
2022-10-27 22:13:51 +0300

    commit

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public int CurrentIndex { get; private set; }
+
+    [SerializeField] private Text _info;
+
+    private int _numberOfPrefabs;
+
+    public void Inititalize(int numberOfPrefabs)
+    {
+        _numberOfPrefabs = numberOfPrefabs;
+    }
+
+    public void SwapLeft()
+    {
+        CurrentIndex = Mathf.Clamp(CurrentIndex - 1, 0, _numberOfPrefabs - 1);
+        UpdateTextInfo();
+    }
+
+    public void SwapRight()
+    {
+        CurrentIndex = Mathf.Clamp(CurrentIndex + 1, 0, _numberOfPrefabs - 1);
+        UpdateTextInfo();
+    }
+
+    private void UpdateTextInfo()
+    {
+        _info.text = CurrentIndex.ToString();
+    }
+}
